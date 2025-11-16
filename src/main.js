333f171fa574e0457e4a70d065ae8cb3de20baf7
Benjamin <jennifer.bhuta@gmail.com>
2025-11-16 18:57:23 -0500

    Create first draft of game

--- src/main.js
+++ src/main.js
@@ -1,7 +1,111 @@
-import k from "./context";
+import kaplay from "kaplay";
 
-k.loadSprite("Mr-Flappy", "sprites/flappy.png")
+const FLOOR_HEIGHT = 48;
+const JUMP_FORCE = 900;
+const SPEED = 480;
 
-k.add([
-    k.sprite("Mr-Flappy")
-])
+// initialize context
+kaplay();
+
+// load assets
+loadSprite("flappy", "sprites/flappy.png");
+
+scene("game", () => {
+    // define gravity
+    setGravity(2700);
+
+    // add a game object to screen
+    const player = add([
+        // list of components
+        sprite("flappy"),
+        scale(3),
+        pos(80, 40),
+        anchor("center"),
+        area({scale: 0.8}),
+        body(),
+         
+    ]);
+
+    // floor
+    add([
+        rect(width(), FLOOR_HEIGHT),
+        outline(4),
+        pos(0, height()),
+        anchor("botleft"),
+        area(),
+        body({ isStatic: true }),
+        color(127, 200, 255),
+    ]);
+
+    function jump() {
+        if (player.isGrounded()) {
+            player.jump(JUMP_FORCE);
+        }
+    }
+
+    // jump when user press space
+    onKeyPress("space", jump);
+    onClick(jump);
+
+    function spawnTree() {
+        // add tree obj
+        add([
+            rect(48, rand(32, 96)),
+            area(),
+            outline(4),
+            pos(width(), height() - FLOOR_HEIGHT),
+            anchor("botleft"),
+            color(255, 180, 255),
+            move(LEFT, SPEED),
+            "tree",
+        ]);
+
+        // wait a random amount of time to spawn next tree
+        wait(rand(0.5, 1.5), spawnTree);
+    }
+
+    // start spawning trees
+    spawnTree();
+
+    // lose if player collides with any game obj with tag "tree"
+    player.onCollide("tree", () => {
+        // go to "lose" scene and pass the score
+        go("lose", score);
+        burp();
+        addKaboom(player.pos);
+    });
+
+    // keep track of score
+    let score = 0;
+
+    const scoreLabel = add([text(score), pos(24, 24)]);
+
+    // increment score every frame
+    onUpdate(() => {
+        score++;
+        scoreLabel.text = score;
+    });
+});
+
+scene("lose", (score) => {
+    add([
+        sprite("flappy"),
+        pos(width() / 2, height() / 2 - 80),
+        scale(10),
+        anchor("center"),
+    ]);
+
+    // display score
+    add([
+        text(score),
+        pos(width() / 2, height() / 2 + 80),
+        scale(2),
+        anchor("center"),
+    ]);
+
+    // go back to game with space is pressed
+    onKeyPress("space", () => go("game"));
+    onClick(() => go("game"));
+});
+
+go("game");
