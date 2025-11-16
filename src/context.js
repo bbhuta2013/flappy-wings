f82f2ec553d1e6b7e46e439380897e1525c1ded6
Benjamin <jennifer.bhuta@gmail.com>
2025-11-16 18:36:41 -0500

    added mr flappy to game

--- src/context.js
+++ src/context.js
@@ -3,6 +3,13 @@ import kaplay from "kaplay";
 const k = kaplay(
     {
         background: [32, 230, 228],
+        buttons: {
+            jump: {
+                keyboard: ["w"],
+                mouse: "left"
+            }
+        },
+        touchToMouse: true
     }
 )
 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import k from "./context";
+
+k.loadSprite("Mr-Flappy", "sprites/flappy.png")
+
+k.add([
+    k.sprite("Mr-Flappy")
+])
